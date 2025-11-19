20c3bb5e84f1a6b9304eff5bcd4e0da600311ab3
Stefan <hanousek.stefan@hotmail.com>
2025-11-19 14:20:55 +0100

    added file input class and filereader

---
+++ src/FileInput.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class FileInput {
+    public String personList;
+
+    public String[] dataFill;
+    public HashMap<String, Person> personHashMap = new HashMap<>();
+
+
+    public void importPersons() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("data/persons.csv"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.isBlank()) continue;
+                dataFill = line.split(";");
+
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void importAddresses() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("data/addresses.csv"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.isBlank()) continue;
+                dataFill = line.split(";");
+
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void importPersonManagements() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("data/personManagements.csv"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.isBlank()) continue;
+                dataFill = line.split(";");
+
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
