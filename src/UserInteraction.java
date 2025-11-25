ab41e6156d283cdc51e60a4b194f5e2c445aca1b
Stefan <hanousek.stefan@hotmail.com>
2025-11-25 15:27:28 +0100

    ui bauen und so

--- src/UserInteraction.java
+++ src/UserInteraction.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserInteraction implements DateMatcher {
+    Scanner scanner = new Scanner(System.in);
+
     private static final String NAME_REGEX = "^\\p{L}+([\\p{L}\\p{M}'\\- ]*\\p{L})?$";
     private static final Pattern DATE_PATTERN = Pattern.compile(
             "^((2000|2400|2800|(19|2[0-9])(0[48]|[2468][048]|[13579][26]))-02-29)$"
                     + "|^(((19|2[0-9])[0-9]{2})-02-(0[1-9]|1[0-9]|2[0-8]))$"
                     + "|^(((19|2[0-9])[0-9]{2})-(0[13578]|10|12)-(0[1-9]|[12][0-9]|3[01]))$"
                     + "|^(((19|2[0-9])[0-9]{2})-(0[469]|11)-(0[1-9]|[12][0-9]|30))$");
-
-    Scanner scanner = new Scanner(System.in);
 
     public String[] scannerPersonData() {
         String[] personData = new String[5];
@@ -78,6 +78,37 @@ public class UserInteraction implements DateMatcher {
             }
         } while (!success);
         return personData;
+    }
+
+    public void printAnnoyingLoadingBar() throws InterruptedException {
+        System.out.print("\nLoading: ");
+        System.out.print("▓▓");
+        Thread.sleep(450);
+        System.out.print("▓▓▓▓▓▓");
+        Thread.sleep(850);
+        System.out.print("▓▓▓▓");
+        Thread.sleep(300);
+        System.out.print("▓▓▓");
+        Thread.sleep(450);
+        System.out.print("▓▓▓▓▓▓");
+        Thread.sleep(850);
+        System.out.print("▓▓▓▓");
+        Thread.sleep(300);
+        System.out.println("▓▓▓▓▓▓");
+        System.out.println("Loading successful");
+    }
+
+    public void printMainMenu() {
+        System.out.println("""
+                Whats on your mind?
+                1 -> add new Person
+                2 -> print List
+                3 -> sync File with new entries
+                :q! -> Close Program
+                """);
+    }
+    public String getMenuSelect (){
+        return scanner.nextLine();
     }
 
     @Override
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,7 +1,19 @@
 public class UserInterface {
-    public void startUserInterface (){
+    public void startUserInterface () throws InterruptedException {
+        UserInteraction userInteraction = new UserInteraction();
         boolean run = true;
+        String select;
 
+        userInteraction.printAnnoyingLoadingBar();
+        while (run){
+            userInteraction.printMainMenu();
+            select = userInteraction.getMenuSelect();
+//            switch ()
+
+
+
+            run = false;
+        }
 
     }
 }
