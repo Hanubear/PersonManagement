95c7907a00950e2398bc72feb2eaeffa37bceb4a
Stefan <hanousek.stefan@hotmail.com>
2025-11-26 09:16:00 +0100

    comments

--- src/PersonManagement.java
+++ src/PersonManagement.java
@@ -7,20 +7,20 @@ public class PersonManagement {
         return personMgmt;
     }
 
-    public int createPersonManagement(String name) {
-        int key = dataManagement.continuousPersonMgmtID;
-        dataManagement.personManagementHashMap.put(key, name);
-        dataManagement.continuousPersonMgmtID++;
-        return dataManagement.continuousPersonMgmtID;
-    }
-
-    public String get(int personManagementId){
-        try {
-            return dataManagement.personManagementHashMap.get(personManagementId);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    public int createPersonManagement(String name) {
+//        int key = dataManagement.continuousPersonMgmtID;
+//        dataManagement.personManagementHashMap.put(key, name);
+//        dataManagement.continuousPersonMgmtID++;
+//        return dataManagement.continuousPersonMgmtID;
+//    }
+//
+//    public String get(int personManagementId){
+//        try {
+//            return dataManagement.personManagementHashMap.get(personManagementId);
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
 //    public int update(PersonManagement personManagement);    //liefert die Anzahl der aktualisierten Zeilen
 //    public PersonManagement delete(int personManagementId);  //liefert das gelöschte Objekt
